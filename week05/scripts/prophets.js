3bc8d8abdbbc5e43da6a1fbc6fb858a9b93c42de
mistShad <nwa22001@byui.edu>
2023-10-11 23:26:00 +0100

    Added Page Visits

---
+++ week05/scripts/prophets.js
@@ -0,0 +1,43 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProphetData(url) {
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+        // console.table(data);
+        displayProphets(data.prophets);
+    }
+}
+
+getProphetData(url);
+
+const displayProphets = (prophets) => {
+    prophets.forEach(prophet => {
+        let card = document.createElement('section');
+
+        let fullName = document.createElement('h2');
+        fullName.textContent = `${prophet['name']} ${prophet['lastname']}`;
+
+        let birthDate = document.createElement('p');
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+
+        let birthPlace = document.createElement('p');
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        let portrait = document.createElement('img');
+        portrait.setAttribute('src', `${prophet['imageurl']}`);
+        portrait.setAttribute('alt', 'Portrait of a prophet');
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('wdith', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
